3311f2e503bf3fd2fcacb49a6e2e8fa7b7aca108
roysez <sergiobaluh@gmail.com>
2017-10-08 01:40:47 +0300

    DOBRUY RANOK

---
+++ Assets/_main/_scripts/keyboard/KeyboardDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardDamage : MonoBehaviour {
+
+	// Use this for initialization
+	public LayerMask _enemyMask;
+	public ParticleSystem _damageParticles;
+
+	public float _damage = 50f;
+	public float _maxLifeTime = 2f;
+
+	public float _thrust = 1;
+
+	void Start () 
+	{
+		Destroy(gameObject, _maxLifeTime);
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) 
+	{
+		Collider[] colliders = Physics.OverlapBox (transform.position, transform.lossyScale / 2, transform.rotation, _enemyMask);
+		for (int i = 0; i < colliders.Length; ++i) 
+		{
+			Vector3 force = colliders [i].transform.position - transform.position;
+
+			Rigidbody targetRigitbody = colliders [i].GetComponent<Rigidbody> ();
+			if (!targetRigitbody) 
+			{
+				continue;
+			}
+			targetRigitbody.AddForce (force * _thrust);
+
+			//EnemyHealth targetHeath = targetRigitbody.GetComponent<EnemyHealth> ();
+
+
+
+		}
+		Destroy (gameObject);
+	}
+	Vector3 getForce(Vector3 from , Vector3 to)
+	{
+		return to - from;
+	}
+}
--- Assets/_main/_scripts/player/PlayerAttack.cs
+++ Assets/_main/_scripts/player/PlayerAttack.cs
@@ -1,31 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour {
 
-	public float timeBetweenAttacks = 0.5f;
-	float timer;
-	Animator animator;
+	public Rigidbody _keyboard;            
+	public Transform _fireTransform;    
+	public Slider _powerSlider; 
 
+	public float _minLaunchForce = 15f; 
+	public float _maxLaunchForce = 30f; 
+	public float _maxChargeTime = 0.75f;
+
+	public float _timeBetweenAttacks = 0.5f;
+	float _timer;
+	Animator _animator;
+
+	private string _cireButton;         
+	private float _currentLaunchForce;  
+	private float _chargeSpeed;         
+	private bool _fired;              
+
+	private void OnEnable()
+	{
+		_currentLaunchForce = _minLaunchForce;
+		_powerSlider.value = _minLaunchForce;
+	}
 	// Use this for initialization
-	void Start () {
-		animator = GetComponent<Animator> ();	
+	private void Start () 
+	{
+		_animator = GetComponent<Animator> ();	
+
+		_chargeSpeed = (_maxLaunchForce - _minLaunchForce) / _maxChargeTime;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		timer += Time.deltaTime;
+	private void Update ()
+	{
+		_timer += Time.deltaTime;
 
-		if(timer >= timeBetweenAttacks && Input.GetMouseButtonDown (0))
-		{
+		if (_timer >= _timeBetweenAttacks && Input.GetMouseButtonDown (0)) {
 			Attack ();
 		}
+
+			tryToFire ();
 	}
 
-	void Attack(){
-		timer = 0f;
+	private void Attack()
+	{
+		_timer = 0f;
 		Debug.Log ("Attack");
-		animator.SetTrigger ("Attack");
+		_animator.SetTrigger ("Attack");
 	}
+		
+	private void tryToFire()
+	{
+		_powerSlider.value = _minLaunchForce;
+
+		if (_currentLaunchForce >= _maxLaunchForce && !_fired)
+		{
+			_currentLaunchForce = _maxLaunchForce;
+			Fire ();
+		}
+		else if (Input.GetMouseButtonDown(1))
+		{
+			_fired = false;
+			_currentLaunchForce = _minLaunchForce;
+		}
+		else if (Input.GetMouseButton (1) && !_fired)
+		{
+			_currentLaunchForce += _chargeSpeed * Time.deltaTime;
+			_powerSlider.value = _currentLaunchForce;
+		}
+		else if (Input.GetMouseButtonUp (1) && !_fired)
+		{
+			Fire ();
+		}
+	}
+	private void Fire()
+	{
+		// Instantiate and launch the shell.
+
+		Debug.Log ("Fire");
+		_fired = true;
+
+		Rigidbody shellInstance = Instantiate (_keyboard, _fireTransform.position, _fireTransform.rotation) as Rigidbody;
+		Debug.Log (shellInstance.transform.position);
+		shellInstance.velocity = _currentLaunchForce * _fireTransform.forward;
+	
+		_currentLaunchForce = _minLaunchForce;
+	}
+
 }
