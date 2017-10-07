c6f2496b34c227cddfd5fcd3f32abf11f1c4d276
roysez <sergiobaluh@gmail.com>
2017-10-07 21:45:07 +0300

    attacking

---
+++ Assets/_main/_scripts/player/PlayerAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			
+			Attack ();
+				
+		}
+	}
+
+	void Attack(){
+		Debug.Log ("Attack");
+		animator.SetTrigger ("Attack");
+	}
+}
--- Assets/_main/_scripts/player/PlayerController.cs
+++ Assets/_main/_scripts/player/PlayerController.cs
@@ -3,42 +3,104 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float walkSpeed = 2;
-	public float runSpeed = 6;
+	public float _forceJumpConst = 1f;
+	public float _walkSpeed = 2;
+	public float _runSpeed = 6;
 
-	public float turnSmoothTime = 0.2f;
-	float turnSmoothVelocity;
+	public float _turnSmoothTime = 0.2f;
+	float _turnSmoothVelocity;
 
-	public float speedSmoothTime = 0.1f;
-	float speedSmoothVelocity;
-	float currentSpeed;
+	public float _speedSmoothTime = 0.1f;
+	float _speedSmoothVelocity;
+	float _currentSpeed;
+	float _groundDistance = 0.1f;
 
-	Animator animator;
-	Transform cameraT;
+	Animator _animator;
+	Transform _cameraT;
 
-	void Start () {
-		animator = GetComponent<Animator> ();
-		cameraT = Camera.main.transform;
+	private Rigidbody _rigidbody;
+	private GameObject _raycastPoint;
+
+	private bool _canJump;
+
+	Vector2 _axis;
+	bool _running;
+
+	void Start ()
+	{
+		_raycastPoint = GameObject.Find("RaycastPoint");
+		_canJump = false;
+		_running = false;
+		_animator = GetComponent<Animator> ();
+		_cameraT = Camera.main.transform;
+		_rigidbody = GetComponent<Rigidbody>();
 	}
 
-	void Update () {
+	void Update () 
+	{
+		GetAllInput();
+		tryToJump ();
+	}
 
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("HorizontalPlayer"), Input.GetAxisRaw ("VerticalPlayer"));
-		Vector2 inputDir = input.normalized;
+	void FixedUpdate()
+	{
+		move ();
+	}
 
-		if (inputDir != Vector2.zero) {
-			float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+	void GetAllInput()
+	{
+		_axis = new Vector2 (Input.GetAxisRaw ("HorizontalPlayer"), Input.GetAxisRaw ("VerticalPlayer"));		
+		_running = Input.GetKey (KeyCode.LeftShift);
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			_canJump = true;
+		}
+	}
+	void tryToJump()
+	{
+		if(_canJump && IsGrounded())
+		{
+			Debug.Log ("Jump");
+
+			_canJump = false;
+			_rigidbody.AddForce(0f, _forceJumpConst, 0f, ForceMode.Impulse);
+			// _animator.SetTrigger ("Jump");
+		}
+	}
+	bool IsGrounded ()
+	{
+		RaycastHit hit;
+		Ray landingRay = new Ray (_raycastPoint.transform.position, Vector3.down);
+
+		if (Physics.Raycast (landingRay, out hit, _groundDistance + 0.3f)) 
+		{
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+		
+	private void move()
+	{
+		Vector2 inputDir = _axis.normalized;
+		_canJump = Input.GetKey (KeyCode.Space);
+
+		if (inputDir != Vector2.zero) 
+		{
+			float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + _cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref _turnSmoothVelocity, _turnSmoothTime);
 		}
 
-		bool running = Input.GetKey (KeyCode.LeftShift);
-		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-		currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+		float targetSpeed = ((_running) ? _runSpeed : _walkSpeed) * inputDir.magnitude;
+		_currentSpeed = Mathf.SmoothDamp (_currentSpeed, targetSpeed, ref _speedSmoothVelocity, _speedSmoothTime);
 
-		transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
+		transform.Translate (transform.forward * _currentSpeed * Time.deltaTime, Space.World);
 
-		float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
-		animator.SetFloat ("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
-
+		float animationSpeedPercent = ((_running) ? 1 : .5f) * inputDir.magnitude;
+		_animator.SetFloat ("speedPercent", animationSpeedPercent, _speedSmoothTime, Time.deltaTime);
 	}
+
+
+
 }
